dec339b5688251aeded22a223053d60a07cdb1db
aixlau <aixlau@outlook.com>
2023-12-20 17:27:59 +0800

    .

--- Loon/Scrpit/daxuesoutijiang.js
+++
@@ -1,7 +0,0 @@
-let response = JSON.parse($response.body);
-if (response.data && response.data.bannerList) {
-    delete response.data.bannerList; 
-} else if (response.data && response.data.banner) {
-    delete response.data.banner;
-}
-$done({body: JSON.stringify(response)});
